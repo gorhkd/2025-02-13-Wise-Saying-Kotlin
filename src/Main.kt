4bd92dd389fa0ed632baf9893b24d2efd6ed7979
gorhkd <gorhkd223@gmail.com>
2025-02-14 17:39:50 +0900

    1단계 : 종료

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,13 @@
 fun main() {
-    println("== 명언 앱 ==")
-    println("명령) 종료")
+    while (true) {
+        println("== 명언 앱 ==")
+        print("명령) ")
+        val cmd = readln()
+
+        if (cmd == "종료") {
+            println("프로그램을 종료합니다.")
+            break
+        }
+    }
+
 }
